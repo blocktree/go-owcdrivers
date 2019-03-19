a25b127882a7162aa7492bf6d7bb41a8484d6b63
麦志泉 <zhiquan911@qq.com>
2019-03-19 18:23:56 +0800

    增加SignTxHash方法

---
+++ signatureSet/signature.go
@@ -0,0 +1,57 @@
+package signatureSet
+
+import (
+	"fmt"
+	"github.com/blocktree/go-owcdrivers/btcTransaction"
+	"github.com/blocktree/go-owcrypt"
+	"math/big"
+	"strings"
+)
+
+//signTxHash 签名交易单哈希
+func SignTxHash(symbol string, msg []byte, privateKey []byte, eccType uint32) ([]byte, error) {
+	var sig []byte
+	if strings.EqualFold(symbol, "ETH") {
+		sig, err := owcrypt.ETHsignature(privateKey, msg)
+		if err != owcrypt.SUCCESS {
+			return nil, fmt.Errorf("ETH sign hash failed")
+		}
+		return sig, nil
+	}
+
+	if strings.EqualFold(symbol, "NAS") {
+		sig, err := owcrypt.NAS_signature(privateKey, msg)
+		if err != owcrypt.SUCCESS {
+			return nil, fmt.Errorf("NAS sign hash failed")
+		}
+		return sig, nil
+	}
+
+	if strings.EqualFold(symbol, "VSYS") {
+		sig, err := VSYSSignature(privateKey, msg)
+		if err != owcrypt.SUCCESS {
+			return nil, fmt.Errorf("VSYS sign hash failed")
+		}
+		return sig, nil
+	}
+
+	sig, err := owcrypt.Signature(privateKey, nil, 0, msg, 32, eccType)
+	if err != owcrypt.SUCCESS {
+		return nil, fmt.Errorf("ECC sign hash failed")
+	}
+	sig = serilizeS(sig)
+	return sig, nil
+}
+
+func serilizeS(sig []byte) []byte {
+	s := sig[32:]
+	numS := new(big.Int).SetBytes(s)
+	numHalfOrder := new(big.Int).SetBytes(btcTransaction.HalfCurveOrder)
+	if numS.Cmp(numHalfOrder) > 0 {
+		numOrder := new(big.Int).SetBytes(btcTransaction.CurveOrder)
+		numS.Sub(numOrder, numS)
+
+		return append(sig[:32], numS.Bytes()...)
+	}
+	return sig
+}
