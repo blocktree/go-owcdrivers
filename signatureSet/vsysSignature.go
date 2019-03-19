83898cd78e53465376d496a7f8d7c9472c0de6f5
heshuchao <heshuchao@hotmail.com>
2019-03-19 12:00:15 +0800

    add vsys signature

---
+++ signatureSet/vsysSignature.go
@@ -0,0 +1,9 @@
+package signatureSet
+
+import (
+	owcrypt "github.com/blocktree/go-owcrypt"
+)
+
+func VSYSSignature(prikey, msg []byte) ([]byte, uint16) {
+	return owcrypt.Signature(prikey, nil, 0, msg, uint16(len(msg)), owcrypt.ECC_CURVE_ED25519_REF10)
+}
