48c7eaa141f12809bbc55d14e4d535daeb60d7fd
heshuchao <heshuchao@hotmail.com>
2019-10-14 10:23:30 +0800

    fix bug :   64: non-mandatory-script-verify-flag (Non-canonical DER signature). Code:-26"

--- bitcoinsvTransaction/SignaturePubkey.go
+++ bitcoinsvTransaction/SignaturePubkey.go
@@ -57,7 +57,7 @@ func (sp SignaturePubkey) encodeSignatureToScript(sigType byte) []byte {
 		r = append([]byte{0x00}, r...)
 	} else {
 		for i := 0; i < 32; i++ {
-			if r[i] == 0 && r[i+1]&0x80 != 0x80 {
+			if r[0] == 0 && r[1]&0x80 != 0x80 {
 				r = r[1:]
 			} else {
 				break
@@ -68,7 +68,7 @@ func (sp SignaturePubkey) encodeSignatureToScript(sigType byte) []byte {
 		s = append([]byte{0}, s...)
 	} else {
 		for i := 0; i < 32; i++ {
-			if s[i] == 0 && s[i+1]&0x80 != 0x80 {
+			if s[0] == 0 && s[1]&0x80 != 0x80 {
 				s = s[1:]
 			} else {
 				break
